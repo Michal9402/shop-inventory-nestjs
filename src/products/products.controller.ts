733107f1dc9e4a384b1e910c32e2e7124c41875c
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-11 19:01:24 +0100

    update swagger for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,18 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, ParseFloatPipe, UsePipes, ParseIntPipe, Query, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, BadRequestException } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { ProductEntity } from './entities/product.entity';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create new product' })
+  @ApiCreatedResponse({ type: ProductEntity })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get list of products' })
+  @ApiOkResponse({ 
+    type: ProductEntity,
+    isArray: true,
+    description: 'Products list fetched'
+  })
+  @ApiQuery({
+    name: 'catalogId',
+    required: false,
+    description: 'ID of the catalog to filter products',
+    type: Number,
+  })
+  @ApiBadRequestResponse({
+    description: 'catalogId is invalid'
+  })
   findAll(@Query('catalogId') catalogIdString?: string) {
     if (catalogIdString) {
       const catalogId = Number.parseInt(catalogIdString, 10);
@@ -27,16 +46,25 @@ export class ProductsController {
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get product by id' })
+  @ApiOkResponse({ type: ProductEntity })
+  @ApiNotFoundResponse({ description: "Product with id: ${id} doesn't exists" })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update product' })
+  @ApiOkResponse({ type: ProductEntity })
+  @ApiNotFoundResponse({ description: "Product with id: ${id} doesn't exists" })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete product' })
+  @ApiOkResponse({ type: ProductEntity })
+  @ApiNotFoundResponse({ description: "Product with id: ${id} doesn't exists" })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
