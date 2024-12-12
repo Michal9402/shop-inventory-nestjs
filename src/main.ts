87da510049e360ec2afd48a80f2437a19d714f97
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-12 17:08:37 +0100

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('v1');
+  app.use(helmet);
 
   const config = new DocumentBuilder()
     .setTitle('Shop Inventory API')
