1d1106947e5247fdd0ca0f063e92dfb99a627f76
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-12 17:05:25 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('v1');
 
   const config = new DocumentBuilder()
