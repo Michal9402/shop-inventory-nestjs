edcee534144556eab6b3992e9d0310a0c102013d
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-12 17:33:14 +0100

    check if helmet is blocking swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +18,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // app.use(helmet());
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
