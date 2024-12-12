71bd6610ed37da403e17b4b631be5c1667951648
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-12 17:31:52 +0100

    swag

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-    ],
-  });
+  SwaggerModule.setup('api', app, document);
 
   // app.use(helmet());
 
