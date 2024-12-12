1c5a926c41ee1e4c910f226b5e14e9885e6e6fd9
Michal Czarkowski <m.czarkowski94@gmail.com>
2024-12-12 17:41:56 +0100

    add throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { CatalogsModule } from './catalogs/catalogs.module';
 import { DatabaseModule } from './database/database.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ProductsModule, CatalogsModule, DatabaseModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 3000,
+        limit: 5,
+      },
+    ]),
+    ProductsModule,
+    CatalogsModule,
+    DatabaseModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
